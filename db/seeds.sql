75ad041ead1c1e3a0b26621f45321bfcbd80fdd0
Daniel Silber <dsilber@Daniels-MacBook-Pro.local>
2018-03-27 12:24:44 -0700

    Some updates

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Avocado Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Avocado Burger');
+INSERT INTO burgers (burger_name) VALUES ('Avocado Veggie Burger');
